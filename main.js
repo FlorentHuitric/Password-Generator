5e73c733cd8499822fe9eb1791fa94f8f4bc7ad9
FuroRen <florent.huitric@gmail.com>
2022-03-30 23:47:25 +0200

    valid

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 document.body.style.backgroundColor = "black";
 var result = document.getElementById("result");
 var click = document.getElementById('click');
+var length = document.getElementById('length');
 
 // const & var necessary to the generate function
 const allCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789~`!@#$%^&*()_-+={[}]|:;"/'
 
 //function that will generate password_length random password
-function generate(Length) {
+function generate(length) {
     let password = '';
-    for (let i=0; i<Length; i++){
+    for (let i=0; i<length; i++){
         password += allCharacters.charAt(Math.floor(Math.random()*allCharacters.length));
     }             
     return password;
   }
   //event that turns the generated password into text
 click.addEventListener("click", function(){
-    result.innerHTML = generate(16);
+    result.innerHTML = generate(length.value);
 });
 
 
