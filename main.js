ae594d029781de6eb4f1c5bf76bda8c198b5dc37
FuroRen <florent.huitric@gmail.com>
2022-03-30 16:05:21 +0200

    valid

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ var click = document.getElementById('click');
 
 // const & var necessary to the generate function
 const allCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789~`!@#$%^&*()_-+={[}]|:;"/'
-var password = '';
-const password_length = 16;
 
 //function that will generate password_length random password
-function generate(password_length) {
-    for (let i=0; i<password_length; i++){
+function generate(Length) {
+    let password = '';
+    for (let i=0; i<Length; i++){
         password += allCharacters.charAt(Math.floor(Math.random()*allCharacters.length));
-    }
+    }             
     return password;
   }
   //event that turns the generated password into text
-click.addEventListener(onclick, function(){
-    result.innerHTML = generate(password_length);
+click.addEventListener("click", function(){
+    result.innerHTML = generate(16);
 });
+
 
